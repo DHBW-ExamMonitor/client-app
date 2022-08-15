87e93018cb0834196a9ba55936e2e93fa64720b4
Kevin Ludwig <mail@kevinludwig.com>
2022-08-15 15:06:08 +0200

    Fix course modal

--- src/renderer/pages/Courses.tsx
+++ src/renderer/pages/Courses.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { getCourses } from 'renderer/api/courses';
-import AddCourseModal from 'renderer/components/Courses/AddCourseModal';
+import CreateCourseModal from 'renderer/components/Modals/CreateCourseModal';
 import PageLayout from 'renderer/components/PageLayout';
 import { Course } from 'renderer/types/course';
 
@@ -13,7 +13,7 @@ export const Courses: React.FC = () => {
   const [open, setOpen] = React.useState(false);
   return (
     <>
-      <AddCourseModal open={open} setOpen={setOpen} />
+      <CreateCourseModal open={open} setOpen={setOpen} />
       <PageLayout
         title="Kurse"
         subTitle="Alle Kurse in der Übersicht."
