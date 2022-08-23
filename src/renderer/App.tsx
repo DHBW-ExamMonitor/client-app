ebc685ba8d649d2555f48f9dd31a690b2b2f6e30
Kevin Ludwig <mail@kevinludwig.com>
2022-08-23 07:57:04 +0200

    Update App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import Layout from './components/Layout';
+import WarningDialog from './components/Ui/WarningDialog';
 import Courses from './pages/Courses';
 import Dashboard from './pages/Dashboard';
 import Modules from './pages/Modules';
@@ -9,18 +11,29 @@ import Students from './pages/Students';
 import Teilnahmen from './pages/Teilnahmen';
 
 export default function App() {
+  const [open, setOpen] = React.useState(true);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Dashboard pageTitle="Dashboard" />} />
-          <Route path="kurse" element={<Courses />} />
-          <Route path="studenten" element={<Students />} />
-          <Route path="module" element={<Modules />} />
-          <Route path="pruefungstermine" element={<Pruefungstermine />} />
-          <Route path="teilnahmen/:id" element={<Teilnahmen />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <WarningDialog
+        open={open}
+        setOpen={setOpen}
+        title="Achtung"
+        message="Für die Nutzung dieser Software empfiehlt es sich, sich einzucremen."
+        action={() => {}}
+        actionText="Jetzt eincremen"
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Dashboard pageTitle="Dashboard" />} />
+            <Route path="kurse" element={<Courses />} />
+            <Route path="studenten" element={<Students />} />
+            <Route path="module" element={<Modules />} />
+            <Route path="pruefungstermine" element={<Pruefungstermine />} />
+            <Route path="teilnahmen/:id" element={<Teilnahmen />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
