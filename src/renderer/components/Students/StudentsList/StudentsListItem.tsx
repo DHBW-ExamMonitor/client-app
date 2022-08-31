c45a2c2a35a19a913dc77999f41d9fdef533bbff
Kevin Ludwig <mail@kevinludwig.com>
2022-08-31 12:22:06 +0200

    Fix teilnahmen csv

--- src/renderer/components/Students/StudentsList/StudentsListItem.tsx
+++ src/renderer/components/Students/StudentsList/StudentsListItem.tsx
@@ -57,7 +57,7 @@ export const StudentsListItem: React.FC<StudentsListItemProps> = ({
       />
       <tr>
         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-          {student.matrikelnummer}
+          {student?.matrikelnummer}
         </td>
         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
           {student.name}
--- src/renderer/pages/Teilnahmen.tsx
+++ src/renderer/pages/Teilnahmen.tsx
@@ -1,66 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import api from 'renderer/api/api';
 import { getPruefungsteilnahme } from 'renderer/api/pruefungsteilnahme';
 import { getPruefungstermin } from 'renderer/api/pruefungstermine';
-import { getStudent } from 'renderer/api/students';
 import PageLayout from 'renderer/components/PageLayout';
 import Kursliste from 'renderer/components/Pruefungstermine/Kursliste';
 import Termininfo from 'renderer/components/Pruefungstermine/Termininfo';
 import CsvLink from 'renderer/components/Ui/CsvLink';
-import { Pruefungsteilnahmen } from 'renderer/types/pruefungsteilnahme';
-import { Pruefungstermin } from 'renderer/types/pruefungstermin';
 
 /**
  * Teilnahmen Component
  */
 export const Teilnahmen: React.FC = () => {
   const { id } = useParams();
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const { data } = useQuery(['pruefungstermin', id!], ({ queryKey }) =>
-    getPruefungstermin(queryKey[1])
-  );
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const teilnahmen = useQuery(['teilnahmen', id!], ({ queryKey }) =>
-    getPruefungsteilnahme(queryKey[1])
-  );
-
-  const [csvData, setCsvData] = useState<object[]>([]);
-
-  const fillCsvData = async (tn: Pruefungsteilnahmen, d: Pruefungstermin) => {
-    const temp: object[] = [];
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const t of tn) {
-      const student = await getStudent(t.studentId);
-      temp.push({
-        Student: student.name,
-        Pruefungstermin: d.name,
-        Versuch: t.versuch,
-        Status: t.pruefungsteilnahmeStatus,
-        Notizen: t.notizen,
-        Anwesend: '',
-      });
+  const { data } = useQuery(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ['pruefungstermin', id!],
+    ({ queryKey }) => getPruefungstermin(queryKey[1]),
+    {
+      onError: (error) => {
+        console.log(error);
+      },
     }
-    setCsvData(temp);
-  };
-
-  useEffect(() => {
-    if (teilnahmen.data && data) {
-      fillCsvData(teilnahmen.data, data);
+  );
+  const teilnahmen = useQuery(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ['teilnahmen', id!],
+    ({ queryKey }) => getPruefungsteilnahme(queryKey[1]),
+    {
+      onError: (error) => {
+        console.log(error);
+      },
     }
-  }, [teilnahmen, data]);
+  );
+
+  const { data: csvData, isLoading } = useQuery(['csvdata'], async () => {
+    // console.log(tn);
+    const csv = await api.get(`/pruefungstermine/${id}/teilnahmen`);
+    console.log(csv);
+    return csv.data;
+  });
+
+  // // useEffect(() => {
+  // //   mutate(teilnahmen.data ?? []);
+  // // }, [mutate, teilnahmen.data]);
+
+  // useEffect(() => {
+  //   console.log(csvData);
+  // });
 
   return (
     <>
       <PageLayout title="Teilnahmen" subTitle="" hideButton navigateBack>
         <Termininfo termin={data} />
-        <div className="mb-8">
-          <CsvLink
-            data={csvData}
-            filename={`Teilnehmerliste ${data?.name}`}
-            buttonText="Teilnehmerliste herunterladen"
-          />
-        </div>
+        {isLoading ? (
+          <div>Lädt</div>
+        ) : (
+          <div className="mb-8">
+            <CsvLink
+              data={csvData ?? []}
+              filename={`Teilnehmerliste ${data?.name}`}
+              buttonText="Teilnehmerliste herunterladen"
+            />
+          </div>
+        )}
         {data?.kurse.map((kurs) => (
           <Kursliste
             key={kurs.id}
